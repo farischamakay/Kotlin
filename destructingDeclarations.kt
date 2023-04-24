07c0e58cd7a53fc0acb65f70227e5629c85c2e9b
Farischa Makay <57443923+farischamakay@users.noreply.github.com>
2023-04-24 15:45:05 +0700

    Update destructingDeclarations.kt

--- destructingDeclarations.kt
+++ destructingDeclarations.kt
@@ -3,7 +3,15 @@ fun main() {
     var dataUser = DataUser("Fariska",21);
     var (name, age) = dataUser;
     
+    // apabila menggunakan ComponentN
+    /* 
+    val name = dataUser.component1();
+    val age = dataUser.component2();
+    */
+    
     print("My name is $name and my age $age years old");
+    
+    
 }
 
 data class DataUser(val x : String, val y : Int)
